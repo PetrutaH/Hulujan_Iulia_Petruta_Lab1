21509fd6c354b8a2bfe5e064185a48805de02ffc
PetrutaH <167861117+PetrutaH@users.noreply.github.com>
2025-11-24 20:11:59 +0200

    Nou controller cu 3 metode

---
+++ Controllers/newController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hulujan_Iulia_Petruta_Lab1.Controllers
+{
+    public class newController : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit!";
+        }
+
+        public string Salut()
+        {
+            return "Salut!";
+        }
+
+        public string Mesaj(string nume, int varsta)
+        {
+            return $"Salut {nume}. Ai {varsta} ani.";
+        }
+
+    }
+}
