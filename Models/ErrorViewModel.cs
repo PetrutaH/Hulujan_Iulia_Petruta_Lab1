2eed3fb53c4721ec4a1af2e4905a10a1e1aec503
PetrutaH <167861117+PetrutaH@users.noreply.github.com>
2025-11-24 19:45:28 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hulujan_Iulia_Petruta_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
